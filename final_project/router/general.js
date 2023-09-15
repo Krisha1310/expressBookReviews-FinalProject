57d51805d93b73cea2867dd82c766fafaa36ae29
Krisha1310 <krishajetani18@gmail.com>
2023-09-15 16:36:47 -0400

    NEW

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -22,42 +22,54 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  let mypromise = new Promise((resolve,reject => {
-    setTimeout(() => {
-        resolve(JSON.stringify(books))
-      },6000)})
-  );
-  mypromise.the((data)=>{
-      return res.status(300).send(data);
-  });
+  let mypromise = new Promise((resolve,reject) => {
+        resolve(JSON.stringify(books,null,2))
+    });
+    mypromise.then((data)=>{
+    return res.status(300).send(data);
+    });
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-    return res.status(300).json(books[req.params.isbn]);
+    let mypromise = new Promise((resolve,reject) => {
+        resolve(JSON.stringify(books[req.params.isbn],null,2))
+    });
+    mypromise.then((data)=>{
+        return res.status(300).send(data);
+    });
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  let arr = new Array();
-  let keys = Object.keys(books);
-   keys.forEach(obj => {
-       arr.push(books[obj]);
-   });
-   let filteredByAuth = arr.filter(o => o.author.toLowerCase() == req.params.author.toLowerCase());
-  return res.status(300).json(filteredByAuth);
+    let mypromise = new Promise((resolve,reject) => {
+        let arr = new Array();
+        let keys = Object.keys(books);
+        keys.forEach(obj => {
+            arr.push(books[obj]);
+        });
+        let filteredByAuth = arr.filter(o => o.author.toLowerCase() == req.params.author.toLowerCase());
+        resolve(JSON.stringify(filteredByAuth,null,2))
+    });
+    mypromise.then((data)=>{
+        return res.status(300).send(data);
+    });
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-    let arr = new Array();
-    let keys = Object.keys(books);
-     keys.forEach(obj => {
-         arr.push(books[obj]);
-     });
-     let filteredByTitle = arr.filter(o => o.title.toLowerCase() == req.params.title.toLowerCase());
-  return res.status(300).json(filteredByTitle);
+    let mypromise = new Promise((resolve,reject) => {
+        let arr = new Array();
+        let keys = Object.keys(books);
+        keys.forEach(obj => {
+            arr.push(books[obj]);
+        });
+        let filteredByTitle = arr.filter(o => o.title.toLowerCase() == req.params.title.toLowerCase());
+        resolve(JSON.stringify(filteredByTitle,null,2))
+    });
+    mypromise.then((data)=>{
+        return res.status(300).send(data);
+    });
 });
 
 //  Get book review
